fdff7a38bd22320b93fc2a82a71aaa790c9a9d76
Ryan Kelly <rykelly@microsoft.com>
2024-02-06 16:04:37 -0800

    Updates

--- azext_edge/edge/providers/check/summary.py
+++ azext_edge/edge/providers/check/summary.py
@@ -1,8 +1,9 @@
 from base64 import b64decode
-from datetime import timedelta, timezone, datetime
-from typing import Any, Dict, Tuple, TypedDict
+from datetime import datetime
+from typing import Any, Dict, Tuple
 from azext_edge.edge.common import CheckTaskStatus
 from azext_edge.edge.util.x509 import get_certificate_expiry
+
 from .base import left_pad
 
 from rich.padding import Padding
@@ -50,51 +51,67 @@ def eval_aio_summary(
 def eval_nodes(
     check_manager: CheckManager, padding: Tuple[int, int, int, int] = (0, 0, 0, 8)
 ) -> None:
-    target = "summary/nodes"
-    check_manager.add_target(target_name=target)
-    # check_manager.add_display(target_name=target, display=Padding("nodes", padding))
-    return
+    pass
 
 
 def eval_secrets(
     check_manager: CheckManager, padding: Tuple[int, int, int, int] = (0, 0, 0, 8)
 ) -> None:
     target = "summary/secrets"
-    secret_prefix = "aio-ca-key-pair"
-    # cm_prefix = "aio-ca-trust-bundle"
-    check_manager.add_target(target_name=target)
-    # check_manager.add_display(target_name=target, display=Padding("secrets", padding))
-
+    check_manager.add_target(target_name=target, namespace=DEFAULT_NAMESPACE)
+    check_manager.add_display(
+        namespace=DEFAULT_NAMESPACE,
+        target_name=target,
+        display=Padding("CA keypair secret checks", padding),
+    )
+    padding = left_pad(padding, 4)
     # try to get secret expiration time
 
     # try to find non-test certificates
-    secret = get_namespaced_secret(namespace=DEFAULT_NAMESPACE, secret_name=secret_prefix)
-    if not secret:
-        secret_prefix = f"{secret_prefix}-test-only"
-        secret = get_namespaced_secret(namespace=DEFAULT_NAMESPACE, secret_name=secret_prefix)
-    secret_crt = b64decode(secret['data']['tls.crt'])
-    # print(secret_crt)
+    secret: dict = None
+    secret_name = ""
+    for _ in ["aio-ca-key-pair", "aio-ca-key-pair-test-only"]:
+        secret_name = _
+        secret = get_namespaced_secret(namespace=DEFAULT_NAMESPACE, secret_name=_)
+        if secret:
+            break
+    if not secret or not secret.get("data", {}).get("tls.crt", None):
+        warning_val = f"No aio-ca-key-pair value found in {DEFAULT_NAMESPACE}"
+        check_manager.add_target_eval(
+            namespace=DEFAULT_NAMESPACE,
+            target_name=target,
+            status=CheckTaskStatus.warning.value,
+            value=warning_val,
+        )
+        check_manager.add_display(
+            namespace=DEFAULT_NAMESPACE, target_name=target, display=Padding(warning_val, padding)
+        )
+        return
+
+    secret_crt = b64decode(secret["data"]["tls.crt"])
+
     expiry = get_certificate_expiry(secret_crt)
     expiry_delta = expiry - datetime.utcnow()
     expiry_delta_days = expiry_delta.days
 
-    # green if > 15 days
+    # TODO - determine if we should show warnings/errors based on percentages or actual days remaining
     status = CheckTaskStatus.success.value
-    # yellow if 3-14 days
-    warning_limit = 14
-    # red if < 3 days
+    warning_limit = 10
     error_limit = 3
     if expiry_delta_days <= error_limit:
         status = CheckTaskStatus.error.value
     elif expiry_delta_days <= warning_limit:
         status = CheckTaskStatus.warning.value
 
-    print(f"expiry: {expiry}")
-    print(f"expires in: {expiry_delta.days} days at {expiry.time()}")
-    print(f"status: {status}")
-    check_manager.add_target_eval(target_name=target, status=status)
-    check_manager.add_display(target_name=target, display=Padding(
-        f"Secret {secret_prefix} expires in: {expiry_delta.days} days at {expiry.time()}",
-        padding
-    ))
+    check_manager.add_target_eval(
+        namespace=DEFAULT_NAMESPACE, target_name=target, status=status
+    )
+    check_manager.add_display(
+        namespace=DEFAULT_NAMESPACE,
+        target_name=target,
+        display=Padding(
+            f"- Secret {secret_name} expires in: {expiry_delta.days} days at {expiry.time()}",
+            padding,
+        ),
+    )
     return
