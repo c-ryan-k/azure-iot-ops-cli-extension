f2155633682819847be7572438c6fc843ece0a51
Ryan K <ryan.k@outlook.com>
2024-04-15 09:53:43 -0700

    wip

--- docker-run-with-kubeconfig.sh
+++ docker-run-with-kubeconfig.sh
@@ -1,8 +1,18 @@
-export azext_edge_cluster=musical-fishstick-4jgr4vq5vrrfj6jj
-export azext_edge_rg=ops-cli-int-test-rg
-export azext_edge_kv=
-export config_path=/root/.kube/config
+export azext_edge_cluster="musical-fishstick-4jgr4vq5vrrfj6jj"
+export azext_edge_rg="ops-cli-int-test-rg"
+export azext_edge_kv=""
+export config_path="/root/.kube/config"
 export local_config="C:\Users\rykelly\.kube\config"
+
+export AZURE_TENANT_ID=""
+export AZURE_CLIENT_ID=""
+export AIO_CLUSTER_NAME=""
+export AIO_RG_NAME=""
+export AIO_KV_ID=""
+export AIO_SP_APP_ID=""
+export AIO_SP_OBJECT_ID=""
+export AIO_SP_SECRET=""
+export CUSTOM_LOCATIONS_OID=""
 
 # init steps
 
