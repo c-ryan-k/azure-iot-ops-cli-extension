179e7697bdbc66f13a6fedf7182e01592bf6f129
Ryan K <ryan.k@outlook.com>
2024-08-27 15:52:50 -0700

    added argument error handling for summary checks

--- azext_edge/edge/commands_edge.py
+++ azext_edge/edge/commands_edge.py
@@ -9,7 +9,7 @@ from os.path import exists
 from pathlib import PurePath
 from typing import Any, Dict, Iterable, List, Optional, Union
 
-from azure.cli.core.azclierror import InvalidArgumentValueError
+from azure.cli.core.azclierror import InvalidArgumentValueError, ArgumentUsageError
 from knack.log import get_logger
 
 from .common import OpsServiceType
@@ -71,6 +71,20 @@ def check(
         run_post = False
     if post_deployment_checks and not pre_deployment_checks:
         run_pre = False
+
+    # error if resource_name provided without ops_service
+    if resource_name and not ops_service:
+        raise ArgumentUsageError(
+            "Resource name filtering (--resource-name) can only be used with service name (--svc)."
+        )
+
+    if resource_kinds and not ops_service:
+        raise ArgumentUsageError(
+            "Service name (--svc) is required to specify individual resource kind checks."
+        )
+
+    if detail_level != ResourceOutputDetailLevel.summary.value and not ops_service:
+        logger.warning("Detail level (--detail-level) will only affect individual service checks with '--svc'")
 
     return run_checks(
         ops_service=ops_service,
